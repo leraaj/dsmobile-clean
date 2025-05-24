5e6c9174c9140c6a7b897fe3f2eee53296987522
leraaj <leraalfredojohn@gmail.com>
2025-05-24 22:37:41 +0800

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'abg-application',
+  webDir: 'dist'
+};
+
+export default config;
